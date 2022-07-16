774b36f162056f97715537f1685175afb74f02d6
nareshsharma0318@gmail.com <nareshsharma0318@gmail.com>
2022-07-16 23:41:59 +0530

    states use calculator

--- 1.ex/src/App.js
+++ 1.ex/src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App(){
+  const [num1, setNum1] = useState();
+  const [num2, setNum2] = useState();
+  const [op, setOp] = useState();
+  const [result, setResult] = useState(0);
+  
+  function handlenum1(e1){
+    setNum1(e1.target.value)
+  }
+  console.log(num1)
+  
+  function handlenum2(e){
+    setNum2(e.target.value)
+  }
+  console.log(num2)
+
+  function handleOp(e){
+    setOp(e.target.value)
+  }
+  console.log(op)
+
+  function handleresult(){
+    if(op === "+"){
+      let ans = Number(num1) + Number(num2)
+      setResult(ans)
+    }
+
+    else if(op === "-"){
+      let ans = Number(num1) - Number(num2)
+      setResult(ans)
+    }
+
+    else if(op === "*"){
+      let ans = Number(num1) * Number(num2)
+      setResult(ans)
+    }
+
+    else if(op === "/"){
+      let ans = Number(num1) / Number(num2)
+      setResult(ans)
+    }
+    
+    
+    // let ans = Number(num1) + Number(num2)
+    // setResult(ans)
+  }
+  console.log(result)
+  return(
+    <div className='cal'><center> 
+      <p>Enter value for num1 = <input type="number" value={num1} onChange={(e1) => handlenum1(e1)}  /></p>
+      <p>Enter value for num2 = <input type="number" value={num2} onChange={(e) => handlenum2(e)}  /></p>
+      <p>select operator <select onChange={(e) => {handleOp(e)}} >
+        <option>+</option>
+        <option>-</option>
+        <option>*</option>
+        <option>/</option>
+      </select>
+      </p>
+      <button type="submit" onClick={handleresult}>result</button>
+      
+      <h1>{result}</h1>
+      </center>
     </div>
-  );
+  )
 }
 
-export default App;
+
+
+
+
+
+
+
+
+
+
+
+// import './App.css';
+// import Fruit from './fruit';
+// import { useEffect } from 'react';
+
+// function App() {
+//   const fruits = {
+//     name : "Naresh Sharma",
+//     fname : "Jagdish Chandra Sharma"
+//   }
+
+//   useEffect(() => {
+//     console.log(fruits)
+// },[])
+ 
+
+//   return (
+//     <div className="App">
+//       <Fruit fruits={fruits} />
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ 1.ex/src/fruit.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+const Fruit = (item) =>{
+
+    
+        
+
+
+    return(
+        <div>
+            <center>
+                <button>
+                <p>Name : {item.fruits.name}</p>
+                <p>F_Name : {item.fruits.fname}</p>
+                </button>
+            </center>
+        </div>
+    )
+}
+
+export default Fruit;
